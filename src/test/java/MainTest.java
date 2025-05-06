47ad69f2b4987ff642e10b0b8cbeb5233b44df2b
Салпагаров Руслан <rusik12346rusik@gmail.com>
2025-05-06 19:18:39 +0300

    set

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
